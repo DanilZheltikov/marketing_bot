23a8580df92a621ea438199b1d6b6d3b315bd481
DanilZheltikov <hikikomoridaz@gmail.com>
2026-05-14 01:15:59 +0700

    feat(mailing_stats): implement MailingStatsRepository for CRUD operations

--- app/core/schemas.py
+++ app/core/schemas.py
@@ -14,3 +14,14 @@ class PostCreate(BaseModel):
     main_post: bool = False
     post_text: str
     step_number: int | None = None
+
+
+class MailingStatsCreate(BaseModel):
+    cold_users: int
+    bot_blocked_users: int
+
+
+class MailingStatsRead(MailingStatsCreate):
+    formatted_date: str
+
+    model_config = ConfigDict(extra='ignore')
---
+++ app/crud_repositories/mailing_stats.py
@@ -0,0 +1,58 @@
+from core.database import BaseRepository
+from core.schemas import MailingStatsCreate, MailingStatsRead
+
+
+class MailingStatsRepository(BaseRepository):
+    """Отвечает за CRUD-операции с сущностью MailingStats в базе SQLite"""
+
+    async def add_mailing_stats(
+        self,
+        mailing_stats_data: MailingStatsCreate
+    ) -> None:
+        """Добавляет статистику рассылок в базу."""
+        await self.db.execute(
+            """--sql
+            INSERT INTO mailing_stats(cold_users, blocked_bot_users)
+            VALUES (:cold_users, :blocked_bot_users)
+            """,
+            mailing_stats_data.model_dump()
+        )
+        await self.db.commit()
+
+    async def get_dates_from_mailing_stats(self) -> list[dict]:
+        """Возвращает список словарей с ID и отформатированной датой."""
+        async with self.db.execute(
+            """--sql
+            SELECT
+                id,
+                strftime('%d.%m.%Y в %H:%M', created_at) as formatted_date
+            FROM mailing_stats
+            ORDER BY created_at DESC;
+            """
+        ) as cursor:
+            rows = await cursor.fetchall()
+
+            return [
+                {'id': row['id'], 'date': row['formatted_date']}
+                for row in rows
+            ]
+
+    async def get_mailing_stats(
+        self,
+        mailing_stats_id: int
+    ) -> MailingStatsRead | None:
+        async with self.db.execute(
+            """--sql
+            SELECT
+                cold_users,
+                blocked_bot_users,
+                strftime('%d.%m.%Y в %H:%M', created_at) as formatted_date
+            FROM mailing_stats
+            WHERE id = ?
+            LIMIT 1;
+            """,
+            (mailing_stats_id,)
+        ) as cursor:
+            row = await cursor.fetchone()
+
+            return MailingStatsRead(**dict(row)) if row else None
