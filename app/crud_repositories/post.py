706bf0a985ac295801d2ae5d58b457e9ab7d17a2
DanilZheltikov <hikikomoridaz@gmail.com>
2026-05-14 01:16:12 +0700

    feat(posts): implement methods to retrieve and remove posts from the database

--- app/crud_repositories/post.py
+++ app/crud_repositories/post.py
@@ -25,3 +25,53 @@ class PostRepository(BaseRepository):
             """,
             post_data.model_dump(exclude_defaults=True)
         )
+
+    async def get_main_post(self) -> str | None:
+        """Возвращает текст поста для главной."""
+        async with self.db.execute(
+            """--sql
+            SELECT post_text
+            FROM posts
+            WHERE main_post = 1
+            LIMIT 1;
+            """
+        ) as cursor:
+            row = await cursor.fetchone()
+
+            return row['post_text'] if row else None
+
+    async def get_warming_post(self, step: int) -> str | None:
+        """Возвращает текст поста прогрева по его номеру."""
+        async with self.db.execute(
+            """--sql
+            SELECT post_text
+            FROM posts
+            WHERE step_number = ?
+            LIMIT 1
+            """,
+            (step,)
+        ) as cursor:
+            row = await cursor.fetchone()
+
+            return row['post_text'] if row else None
+
+    async def remove_main_post(self) -> None:
+        """Удаляет пост главной страницы."""
+        await self.db.execute(
+            """--sql
+            DELETE FROM posts
+            WHERE main_post = 1
+            """
+        )
+        await self.db.commit()
+
+    async def remove_warming_post(self, step: int) -> None:
+        """Удаляет прогревающий пост по его номеру."""
+        await self.db.execute(
+            """--sql
+            DELETE FROM posts
+            WHERE step_number = ?
+            """,
+            (step,)
+        )
+        await self.db.commit()
